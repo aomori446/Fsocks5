8c4580f9e75b116ebbecbd53fe46d6b5a39b4a34
aomori446 <aomori446@gmail.com>
2025-05-01 18:40:18 +0900

    add UDP ASSOCIATE support

--- internal/fsocks5/udp.go
+++ internal/fsocks5/udp.go
@@ -1,38 +1,79 @@
 package fsocks5
 
+import (
+	"bytes"
+	"slices"
+)
+
 /*
 +----+------+------+----------+----------+----------+
-|RSV | FRAG | ATYP | DST.ADDR | DST.PORT |  DATA    |
+|RSV | FRAG | atyp | DST.ADDR | DST.PORT |  DATA    |
 +----+------+------+----------+----------+----------+
 | 2  |  1   |  1   | Variable |    2     | Variable |
 +----+------+------+----------+----------+----------+
 */
 
 type Datagram struct {
-	Address Address
-	Data    []byte
+	addr Address
+	data []byte
 }
 
-/*func ReadOneDatagram(conn *net.UDPConn) (*Datagram, error) {
-	buf := make([]byte, 65536)
-	n, clientAddr, err := conn.ReadFrom(buf)
+func ReadDatagramFrom(buffer []byte) (*Datagram, error) {
+	// read [RSV FRAG atyp]
+	reader := bytes.NewReader(buffer)
+	read, err := readN(reader, 4)
 	if err != nil {
 		return nil, err
 	}
-	if buf[0] != 0x00 && buf[1] != 0x00 {
-		return nil, Fsocks5.FormatErr
+
+	if !slices.Equal(read[:2], []byte{0x00, 0x00}) || read[2] != 0x00 {
+		return nil, FormatErr
 	}
 
-	//not support fragmentation, drop any datagram whose FRAG field is other than X'00'.
-	if buf[2] != 0x00 {
-		return nil, errors.New("not support fragmentation")
+	reader = bytes.NewReader(buffer[4:])
+	datagram := &Datagram{}
+	switch read[3] {
+	case 0x01: // IPv4
+		read, err = readN(reader, 4+2)
+		if err != nil {
+			return nil, err
+		}
+		datagram.addr = IPAddr{ip: read[:4], port: read[4:], atyp: 0x01}
+		datagram.data = buffer[4+4+2:]
+
+	case 0x03: // Domain
+		read, err = readN(reader, 1)
+		if err != nil {
+			return nil, err
+		}
+		addrLen := read[0]
+		read, err = readN(reader, addrLen+2)
+		if err != nil {
+			return nil, err
+		}
+		datagram.addr = NewDomainNameAddr(read)
+		datagram.data = buffer[4+1+addrLen+2:]
+
+	case 0x04: // IPv6
+		read, err = readN(reader, 16+2)
+		if err != nil {
+			return nil, err
+		}
+		datagram.addr = IPAddr{ip: read[:16], port: read[16:], atyp: 0x04}
+		datagram.data = buffer[4+16+2:]
+
+	default:
+		return nil, AddrErr
 	}
+	return datagram, nil
+}
 
-	var address Fsocks5.Address
-	switch buf[3] {
-	case 0x01:
-
-	}
-
-
-}*/
+func (d *Datagram) Bytes() []byte {
+	return append(append([]byte{
+		0x00,           // RSV
+		0x00,           // RSV
+		0x00,           // FRAG
+		d.addr.Atyp()}, // ATYP
+		d.addr.Bytes()...), // DST.ADDR + DST.PORT
+		d.data...) // DATA
+}
